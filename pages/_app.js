c9f3d15ac22a0cbb401389753a28576376527025
Massimo Di Berardino <massimokris22@gmail.com>
2021-10-10 02:55:54 -0300

    Web vitals

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@
 import Layout from "@components/Layout"
 import '../styles.css'
 
+export function reportWebVitals(metric) {
+    console.log(metric)
+}
+
 function MyApp({ Component, pageProps }) {
     return (
         <Layout>
